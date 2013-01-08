b82fe65e75e2eb26078f098a5e551add68746412
Javier <jmolina@paradigmatecnologico.com>
2013-01-08 15:43:49 +0100

    added more test options

--- src/com/paradigmatecnologico/binaryrpc/messagePack/MessagePackTest.java
+++ src/com/paradigmatecnologico/binaryrpc/messagePack/MessagePackTest.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-
 public class MessagePackTest {
 	
 	public static void main(String[] args) throws Exception {
@@ -19,29 +18,41 @@ public class MessagePackTest {
 	    
 		System.out.println("Push 1 to lauch a new call");
 	    System.out.println("Push 2 to lauch a batch test (1000 simultaneous calls)");
+	    System.out.println("Push 3 to show times and reset counters");
 	    System.out.println("Push any other key to exit");
 	    
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    Boolean looping= true;
 	    Integer index = 1;
+	    String input="";
 	    while (looping){
-	    	String input = br.readLine();
-	    	if ("1".equals(input)){
-	    		Thread thread1 = new Thread(new MessagePackTestClient(),"client"+index);
+	    	input = br.readLine();
+
+	    	switch (input) {
+			case "1":
+				Thread thread1 = new Thread(new MessagePackTestClient(),"client"+index);
 	    	    thread1.start();
 	    	    index++;
-	    	}else{
-	    		if ("2".equals(input)){
-	    			index=1;
-	    			for (int i = 0; i < 1000; i++) {
-						Thread thread1 = new Thread(new MessagePackTestClient(),"client"+i);
-						thread1.start();
-					}
-	    			looping=false;
-	    		}else{	    			
-	    			looping=false;
-	    		}	    		
-	    	}
+				break;
+			case "2":
+				index=1;
+    			for (int i = 0; i < 1000; i++) {
+					Thread thread2 = new Thread(new MessagePackTestClient(),"client"+i);
+					thread2.start();
+				}
+				break;
+			case "3":
+				if (MessagePackTestServer.count>0){
+			    	System.out.println("The average time for "+MessagePackTestServer.count +" calls was: "+MessagePackTestServer.time/MessagePackTestServer.count);	    	
+			    }
+				MessagePackTestServer.count=0;
+				MessagePackTestServer.time=new Long(0); 
+				break;
+
+			default:
+				looping=false;
+				break;
+			}
 	    }
 	    
 //	    Thread.currentThread().sleep(240000);
